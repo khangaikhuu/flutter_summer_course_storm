15a27330308cf6f79af02da6f09d5da6e8bd92dc
Teacher Khangaikhuu <gtkhangaikhuu@khangaikhuus-MacBook-Pro.local>
2023-07-08 11:35:17 +0800

    add: day 09;

--- day_09/lib/day_09.dart
+++ day_09/lib/day_09.dart
@@ -1,3 +1,26 @@
-int calculate() {
-  return 6 * 7;
+class Sprite {
+  Sprite(String name){
+    this.name = name;
+  }
+  String name = 'Sprite';
+  int x = 0;
+  int y = 0;
+  bool isShow = true;
+  int size = 100;
+  int direction = 90;
+  void move(int x){
+    this.x = x;
+  }
+  String say(){
+    return this.name;
+  }
+}
+
+void main() {
+  final Luca = Sprite('Luca'); // constructor буюу constructor wihtout parameter
+  final Hat = Sprite('Hat');
+  final Kia = Sprite('Kia');
+  print(Luca.say());
+  print(Hat.say());
+  print(Kia.say());
 }
